9992e428c2d28050fc09d602e37ee0e2301e7790
lamphamz <lamphamondahood@gmail.com>
2025-06-10 13:07:24 +0700

    fix: react import in MembersPage

--- src/pages/Class/MembersPage.tsx
+++ src/pages/Class/MembersPage.tsx
@@ -1,5 +1,4 @@
 // pages/MembersPage.tsx
-import React from 'react'
 import { useParams } from 'react-router-dom'
 import ClassMemberView from './ClassMemberView'
 
@@ -8,14 +7,14 @@ export function MembersPage() {
 
   if (!classId) {
     return (
-      <div className="p-4">
-        <div className="text-red-500">Error: Class ID not found</div>
+      <div className='p-4'>
+        <div className='text-red-500'>Error: Class ID not found</div>
       </div>
     )
   }
 
   return (
-    <div className="p-4">
+    <div className='p-4'>
       <ClassMemberView classId={classId} />
     </div>
   )
